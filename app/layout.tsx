dfe35bb96632e4beee6b8adcd15bbd3ecb83674e
Your Name <you@example.com>
2024-05-26 15:43:50 +0530

    modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +18,6 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
-                <Toaster />
                 <Navbar />
                 {children}
             </body>
